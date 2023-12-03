8bb46cf3a13b2cbc03470253164c80ba360a9b4f
Michael Teye <michaelteyegyemah@gmail.com>
2023-12-03 05:25:59 +0000

    sorted square arr

---
+++ sortedsquarearr.js
@@ -0,0 +1,40 @@
+function sortedSquaredArray(array) {
+    let left = 0;
+    let right = array.length - 1;
+    let result = new Array(array.length);
+  
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (Math.abs(array[left]) > Math.abs(array[right])) {
+        result[i] = array[left] * array[left];
+        left++;
+      } else {
+        result[i] = array[right] * array[right];
+        right--;
+      }
+    }
+  
+    return result;
+  }
+  
+
+
+console.log(sortedSquaredArray([-2,-1]))
+
+//solution Two
+function sortedSquaredArray(array) {
+    // Write your code here.
+    let arrsquard;
+    let changenumber = array.map(number => Math.abs(number))
+    let sortedarr = changenumber.sort((a,b)=>{
+      return a - b;
+    });
+    arrsquard =  sortedarr.map((arr)=>{
+      
+      return arr ** 2;
+    })
+    
+    return arrsquard;
+  }
+  
+  // Do not edit the line below.
+  exports.sortedSquaredArray = sortedSquaredArray;
